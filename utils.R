46cfd7e0638a2cb65da7be059ca4a92d5b8e6f40
Roberto Sánchez Ávalos <r.sanchezavalos@gmail.com>
2018-04-22 19:03:03 +0000

    initial commit

---
+++ utils.R
@@ -0,0 +1,18 @@
+########################################
+## Instalaci  n y preparaci  n de Ambiente
+########################################
+
+# Corre la siguiente funci  n para instalar los paquetes usados en este repositorio
+instalar <- function(paquete) {
+  if (!require(paquete,character.only = TRUE, quietly = TRUE, warn.conflicts = FALSE)) {
+    install.packages(as.character(paquete), repos = "http://cran.us.r-project.org")
+    library(paquete, character.only = TRUE, quietly = TRUE, warn.conflicts = FALSE)
+  }
+}
+
+paquetes <- c('magrittr', 'dplyr', 'tidyr', 'readr',
+              'ggplot2', 'stringr','readxl', 'aws.s3', 'corrplot',
+              'reshape2', 'lubridate', 'Hmisc', 'gridExtra', #'mxmaps', 'mapproj',
+              'BBmisc', 'ggthemes', 'RcppRoll', 'qualvar', 'gridExtra', 'grid')
+
+lapply(paquetes, instalar)
